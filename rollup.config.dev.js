c9e099ce9d944553be05e0d5c400263c4cdb5dbe
Ben Pyrik <ben@wsundine.com>
2018-09-17 21:56:06 -0400

    Split rollup.config.js into two config files, one for development...
    and one for production.

---
+++ rollup.config.dev.js
@@ -0,0 +1,28 @@
+
+import pkg from './package.json';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import serve from 'rollup-plugin-serve';
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: `./docs/hello-world.js`,
+    format: 'umd',
+    // This is the name of the exported global object.
+    // So the world function could be called with hello.world();
+    name: pkg.name
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**'  // only transpile our source code
+    }),
+    serve({
+      open: true,
+      contentBase: './docs'
+    })
+  ]
+}
